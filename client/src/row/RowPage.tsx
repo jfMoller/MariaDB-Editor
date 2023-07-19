bb6e2fc2298c311ff8d336847ae62855bd3fcd68
jm <johan.moller040@gmail.com>
2023-07-19 14:18:32 +0200

    Added redirect on render, components for selected data row

--- client/src/row/RowPage.tsx
+++ client/src/row/RowPage.tsx
@@ -1,30 +1,107 @@
+import { useState } from "react";
 import { useRouteLoaderData } from "react-router-dom";
 
 export default function RowDataList() {
   const rowData = useRouteLoaderData("rowData") as any;
-  console.log(rowData);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editedData, setEditedData] = useState(rowData);
+
+  function handleEdit() {
+    setIsEditing(true);
+  }
+
+  function handleSave() {
+    // Perform save operation with editedData
+    setIsEditing(false);
+  }
+
+  function handleCancel() {
+    setEditedData(rowData);
+    setIsEditing(false);
+  }
+
+  function handleInputChange(event: React.ChangeEvent<HTMLInputElement>) {
+    const { name, value } = event.target;
+    setEditedData((prevData: any) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  }
 
   return (
     <div>
-      <ul className="list-disc pl-6">
-        {Object.entries(rowData).map(([key, value]) => (
-          <li key={key} className="mb-2">
-            <span className="font-bold">{key}: </span>
-            {typeof value === "object" ? (
-              <ul className="list-disc pl-6">
-                {Object.entries(value).map(([nestedKey, nestedValue]) => (
-                  <li key={nestedKey} className="mb-2">
-                    <span className="font-bold">{nestedKey}: </span>
-                    <span>{nestedValue}</span>
-                  </li>
-                ))}
-              </ul>
-            ) : (
-              <span>{value}</span>
-            )}
-          </li>
-        ))}
-      </ul>
+      <div className="grid grid-cols-2 gap-4">
+        {Object.entries(editedData).map(function ([key, value]) {
+          return (
+            <div key={key} className="mb-4">
+              <div className="pl-4">
+                {typeof value === "object" ? (
+                  <ul className="w-full">
+                    {Object.entries(value).map(function ([nestedKey, nestedValue]) {
+                      return (
+                        <li key={nestedKey} className="mb-2">
+                          <span className="font-bold">{nestedKey}: </span>
+                          <br />
+                          {isEditing ? (
+                            <input
+                              type="text"
+                              name={nestedKey}
+                              value={nestedValue}
+                              onChange={handleInputChange}
+                              className="border rounded px-2 py-1"
+                            />
+                          ) : (
+                            <span>{nestedValue}</span>
+                          )}
+                        </li>
+                      );
+                    })}
+                  </ul>
+                ) : (
+                  <p>
+                    {isEditing ? (
+                      <input
+                        type="text"
+                        name={key}
+                        value={value}
+                        onChange={handleInputChange}
+                        className="border rounded px-2 py-1"
+                      />
+                    ) : (
+                      value
+                    )}
+                  </p>
+                )}
+              </div>
+            </div>
+          );
+        })}
+      </div>
+      {isEditing ? (
+        <div className="mt-4">
+          <button
+            onClick={handleSave}
+            className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mr-2"
+          >
+            Save
+          </button>
+          <button
+            onClick={handleCancel}
+            className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Cancel
+          </button>
+        </div>
+      ) : (
+        <div className="mt-4">
+          <button
+            onClick={handleEdit}
+            className="bg-green-500 hover:bg-green-700 text-white font-bold py-2 px-4 rounded"
+          >
+            Edit
+          </button>
+        </div>
+      )}
     </div>
   );
 }
--- client/src/table/TablePage.tsx
+++ client/src/table/TablePage.tsx
@@ -12,75 +12,78 @@ export default function TablePage() {
   ) as any;
 
   useEffect(() => {
-    !table ? navigate(`/${tableTitles[0]}`) : null;
+    !table && !rowID ? navigate(`/${tableTitles[0]}`): null;
   }, []);
 
   const tableData = useRouteLoaderData("tableData") as any;
   const tableContent = tableData ? Object.keys(tableData[0]) : [];
 
-    return (
-      <main className="h-screen w-screen bg-gray-100">
-        <Foldout
-          title={"Row Data"}
-          content={rowID ? <RowPage /> : null}
-          open={rowID !== undefined}
-          onClose={() => {
-            navigate(`/${table}`);
-          }}
-        />
-
-        <header className="sticky top-0 flex items-center justify-between bg-gray-900 py-4 px-6">
-          <h1 className="text-2xl font-bold text-white">{databaseTitle}</h1>
-          <nav className="flex space-x-4">
-            {tableTitles.map((tableTitle) => (
-              <a
-                key={tableTitle}
-                onClick={() => {
-                  navigate(`/${tableTitle}`);
-                }}
-                className="cursor-pointer text-white px-4 py-2 rounded-md bg-gray-800 hover:bg-gray-700 focus:bg-gray-700"
-              >
-                {tableTitle}
-              </a>
-            ))}
-          </nav>
-        </header>
-        <div className="flex flex-col justify-center items-center w-full h-full overflow-auto">
-          <div className="w-screen overflow-x-auto">
-            <table className="w-full text-gray-800 bg-white rounded-md shadow-md">
-              <thead className="sticky top-0">
-                <tr className="bg-gray-200">
-                  {tableContent.map((columnTitle) => (
-                    <th
-                      key={columnTitle}
-                      className="px-6 py-4 text-left font-bold uppercase"
-                    >
-                      {columnTitle}
-                    </th>
-                  ))}
-                </tr>
-              </thead>
-              <tbody>
-                {tableData.map((columnTitle, index) => (
-                  <tr
-                    key={index}
-                    className={index % 2 === 0 ? "bg-gray-100" : ""}
-                    onClick={() => {
-                      navigate(`/${table}/${columnTitle.id}`);
-                    }}
-                  >
-                    {tableContent.map((dataRow) => (
-                      <td key={dataRow} className="px-6 py-4">
-                        {columnTitle[dataRow]}
-                      </td>
-                    ))}
-                  </tr>
-                ))}
-              </tbody>
-            </table>
-          </div>
-        </div>
-      </main>
-    );
+  if (!tableData) {
+    return null;
   }
 
+  return (
+    <main className="h-screen w-screen bg-gray-100">
+      <Foldout
+        title={"Row Data"}
+        content={rowID ? <RowPage /> : null}
+        open={rowID !== undefined}
+        onClose={() => {
+          navigate(`/${table}`);
+        }}
+      />
+
+      <header className="sticky top-0 flex items-center justify-between bg-gray-900 py-4 px-6 min-h-100 max-h-100">
+        <h1 className="text-2xl font-bold text-white">{databaseTitle}</h1>
+        <nav className="flex space-x-4">
+          {tableTitles.map((tableTitle) => (
+            <a
+              key={tableTitle}
+              onClick={() => {
+                navigate(`/${tableTitle}`);
+              }}
+              className="cursor-pointer text-white px-4 py-2 rounded-md bg-gray-800 hover:bg-gray-700 focus:bg-gray-700"
+            >
+              {tableTitle}
+            </a>
+          ))}
+        </nav>
+      </header>
+      <div className="flex flex-col justify-center items-center w-full h-full overflow-auto">
+        <div className="w-screen overflow-x-auto">
+          <table className="w-full text-gray-800 bg-white rounded-md shadow-md">
+            <thead className="sticky top-0">
+              <tr className="bg-gray-200">
+                {tableContent.map((columnTitle) => (
+                  <th
+                    key={columnTitle}
+                    className="px-6 py-4 text-left font-bold uppercase"
+                  >
+                    {columnTitle}
+                  </th>
+                ))}
+              </tr>
+            </thead>
+            <tbody>
+              {tableData.map((columnTitle, index) => (
+                <tr
+                  key={index}
+                  className={index % 2 === 0 ? "bg-gray-100" : ""}
+                  onClick={() => {
+                    navigate(`/${table}/${columnTitle.id}`);
+                  }}
+                >
+                  {tableContent.map((dataRow) => (
+                    <td key={dataRow} className="px-6 py-4">
+                      {columnTitle[dataRow]}
+                    </td>
+                  ))}
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </main>
+  );
+}
